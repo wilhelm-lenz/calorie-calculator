75e9b8b7277d1de6e04afed3b4110029cdbb34e0
wilhelm-lenz <wilhelm.v.lenz@gmail.com>
2023-10-27 08:27:15 +0200

    bugfixing and style update

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,38 +18,59 @@ const calcBasalMetabolicRate = (genderId) => {
   let age = Number(ageElement.value);
   let weight = Number(weightElement.value);
   let resultBaseMetabolicRate = 0;
-  if (genderId === "female") {
-    resultBaseMetabolicRate = (
-      655.1 +
-      9.6 * weight +
-      1.8 * height -
-      4.7 * age
-    ).toFixed(2);
-  } else if (genderId === "male") {
-    resultBaseMetabolicRate = (
-      66.47 +
-      13.7 * weight +
-      5 * height -
-      6.8 * age
-    ).toFixed(2);
+  if (height !== 0 && age !== 0 && weight !== 0) {
+    if (genderId === "female") {
+      resultBaseMetabolicRate = (
+        655.1 +
+        9.6 * weight +
+        1.8 * height -
+        4.7 * age
+      ).toFixed(2);
+    } else if (genderId === "male") {
+      resultBaseMetabolicRate = (
+        66.47 +
+        13.7 * weight +
+        5 * height -
+        6.8 * age
+      ).toFixed(2);
+    }
+    return Number(resultBaseMetabolicRate).toFixed(2);
   }
-  return resultBaseMetabolicRate;
 };
 
 submitBtnElement.addEventListener("click", () => {
   event.preventDefault();
+  baseKcalElement.style.color = "#ffffff";
+  baseKjElement.style.color = "#ffffff";
+  totalKcalElement.style.color = "#ffffff";
+  totalKjElement.style.color = "#ffffff";
+  console.log(calcBasalMetabolicRate(femaleElement.id));
   let pal = palFactorSelectElement.value;
-  if (femaleElement.checked) {
-    baseMetabolicRate = calcBasalMetabolicRate(femaleElement.id);
-    baseKcalElement.textContent = baseMetabolicRate;
-    baseKjElement.textContent = (baseMetabolicRate * 4.184).toFixed(2);
-    totalKcalElement.textContent = (pal * baseMetabolicRate).toFixed(2);
-    totalKjElement.textContent = (pal * baseMetabolicRate * 4.184).toFixed(2);
-  } else if (maleElement.checked) {
-    baseMetabolicRate = calcBasalMetabolicRate(maleElement.id);
-    baseKcalElement.textContent = baseMetabolicRate;
-    baseKjElement.textContent = (baseMetabolicRate * 4.184).toFixed(2);
-    totalKcalElement.textContent = (pal * baseMetabolicRate).toFixed(2);
-    totalKjElement.textContent = (pal * baseMetabolicRate * 4.184).toFixed(2);
+  if (
+    Number(calcBasalMetabolicRate(femaleElement.id)) !== 0 &&
+    Number(calcBasalMetabolicRate(male.id))
+  ) {
+    if (femaleElement.checked) {
+      baseMetabolicRate = calcBasalMetabolicRate(femaleElement.id);
+      baseKcalElement.textContent = baseMetabolicRate;
+      baseKjElement.textContent = (baseMetabolicRate * 4.184).toFixed(2);
+      totalKcalElement.textContent = (pal * baseMetabolicRate).toFixed(2);
+      totalKjElement.textContent = (pal * baseMetabolicRate * 4.184).toFixed(2);
+    } else if (maleElement.checked) {
+      baseMetabolicRate = calcBasalMetabolicRate(maleElement.id);
+      baseKcalElement.textContent = baseMetabolicRate;
+      baseKjElement.textContent = (baseMetabolicRate * 4.184).toFixed(2);
+      totalKcalElement.textContent = (pal * baseMetabolicRate).toFixed(2);
+      totalKjElement.textContent = (pal * baseMetabolicRate * 4.184).toFixed(2);
+    }
+  } else {
+    baseKcalElement.textContent = 0;
+    baseKjElement.textContent = 0;
+    totalKcalElement.textContent = 0;
+    totalKjElement.textContent = 0;
+    baseKcalElement.style.color = "#e03131";
+    baseKjElement.style.color = "#e03131";
+    totalKcalElement.style.color = "#e03131";
+    totalKjElement.style.color = "#e03131";
   }
 });
